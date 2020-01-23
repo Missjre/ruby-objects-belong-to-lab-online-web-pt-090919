0cbee391e75babc89287e4efd7725070fdcd0bcf
Jalysa Reason <jalysareason@gmail.com>
2020-01-23 01:57:50 +0000

    Automatically backed up by Learn

--- spec/post_spec.rb
+++ spec/post_spec.rb
@@ -1,2 +1,17 @@
 require "spec_helper"
 
+class Post
+
+  attr_accessor :title, :author
+  def initialize(title)
+    @title = title
+  end
+
+  def author_name
+    if @author == nil
+      return nil
+    else
+      @author.name
+    end
+  end
+end
